2043c9cd89677f90d0f7ff7682ca062103581b3a
gazy cool <ndefrutitus22@gmail.com>
2023-11-19 16:25:51 -0500

    updates

--- build_image.ps1
+++ build_image.ps1
@@ -3,7 +3,7 @@ docker build \
 --build-arg GITHUB_USERNAME='gazypendragon' \
 --build-arg REPOSITORY_NAME='docker-ecr' \
 #--build-arg WEB_FILE_ZIP='rentzone.zip' \
---build-arg WEB_FILE_UNZIP='rentzone' \
+#--build-arg WEB_FILE_UNZIP='rentzone' \
 --build-arg DOMAIN_NAME='www.ndefrusonsllc.com' \
 --build-arg RDS_ENDPOINT='docker-ecr-db.ctgdsdmazrtg.us-east-1.rds.amazonaws.com' \
 --build-arg RDS_DB_NAME='dadmin' \
